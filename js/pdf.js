63258834d65c7bbf5a55e8d64ea9e95d59de89a1
Jasiel Reyes <67475983+JasielReyes-hub@users.noreply.github.com>
2023-08-04 15:20:44 -0600

    04 Agosto 2023 2.0

--- js/pdf.js
+++ js/pdf.js
@@ -1,11 +1,11 @@
 function downloadPDF() {
     // Ruta al archivo PDF que deseas descargar
-    var pdfUrl = 'pdf/PECIOS.VENDEDOR.pdf';
+    var pdfUrl = 'pdf/CatalogoNovaedd.pdf';
 
     // Crea un enlace de descarga temporal
     var link = document.createElement('a');
     link.href = pdfUrl;
-    link.download = 'PECIOS.VENDEDOR.pdf';
+    link.download = 'CatalogoNovaedd.pdf';
 
     // Simula un clic en el enlace para iniciar la descarga
     link.click();
